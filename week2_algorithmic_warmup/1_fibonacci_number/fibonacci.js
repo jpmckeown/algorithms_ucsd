ff81817eed262ff41354a78fb65b532ccbbee7c9
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2023-04-28 09:38:16 +0100

    Fibonacci

--- week2_algorithmic_warmup/1_fibonacci_number/fibonacci.js
+++ week2_algorithmic_warmup/1_fibonacci_number/fibonacci.js
@@ -1,24 +1,26 @@
-// by Alexander Nikolskiy
+// template by Alexander Nikolskiy
+// Fibonacci calculation by JPM
 
 const readline = require('readline');
 const rl = readline.createInterface({
-    input: process.stdin,
-    terminal: false
+  input: process.stdin,
+  terminal: false
 });
 
 process.stdin.setEncoding('utf8');
 rl.on('line', readLine);
 
 function readLine(line) {
-    console.log(fib(parseInt(line, 10)));
-    process.exit();
+  console.log(fib(parseInt(line, 10)));
+  process.exit();
 }
 
 function fib(n) {
-    // write your code here
-  if (n <= 1) {
-    
+  let fibonacci = [0, 1];
+  for (let index = 2; index <= n; index++) {
+    fibonacci[index] = fibonacci[index - 1] + fibonacci[index - 2];
   }
+  return fibonacci[n]
 }
 
 module.exports = fib;
--- week2_algorithmic_warmup/3_greatest_common_divisor/gcd.js
+++ week2_algorithmic_warmup/3_greatest_common_divisor/gcd.js
@@ -14,13 +14,20 @@ function readLine(line) {
         const a = parseInt(line.toString().split(' ')[0], 10);
         const b = parseInt(line.toString().split(' ')[1], 10);
 
+        console.log(a, b);
         console.log(gcd(a, b));
         process.exit();
     }
 }
 
 function gcd(a, b) {
-    // write your code here
+  let bestSoFar = 0;
+  for (let i = 1; i < a + b; i++) {
+    if (a % i == 0 && b % i == 0) {
+      bestSoFar = i;
+    }    
+  }
+  return bestSoFar;
 }
 
 module.exports = gcd;
