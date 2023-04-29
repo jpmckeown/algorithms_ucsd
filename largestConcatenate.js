c58812f4541606f9e76f2f6e805e2f0f28156326
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2023-04-29 18:48:14 +0100

    show greedy algorithm money change can fail

--- largestConcatenate.js
+++ largestConcatenate.js
@@ -1,5 +1,5 @@
 // algorithm only copes with single-digit numbers!
-const digits = [2, 7, 5, 11, 9];
+let digits = [2, 7, 5, 1, 9];
 
 function largestConcatenate(num) {
    let result = "";
@@ -11,9 +11,29 @@ function largestConcatenate(num) {
       result += maxNum;
 
       num.splice(num.indexOf(maxNum), 1);
-      console.log(maxNum, maxNumIndex, num);
+      // console.log(maxNum, maxNumIndex, num);
    }
    return result;
 }
 
+
+function largestConcatenateRecursive(num) {
+   // console.log(num.length, num);
+   let maxNum, maxNumIndex;
+
+   if (num.length === 0) {
+      return "";
+   }
+   maxNum = Math.max(...num);
+   maxNumIndex = num.indexOf(maxNum);
+   num.splice(num.indexOf(maxNum), 1);
+
+   return maxNum + largestConcatenate(num);
+}
+
+
 console.log(largestConcatenate(digits));
+// cannot call both unless digits initialised again
+digits = [2, 7, 5, 1, 9];
+
+console.log(largestConcatenateRecursive(digits));
