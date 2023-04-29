f3f844846391e86b017fe9a240d1db2102a15534
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2023-04-29 19:27:32 +0100

    completed maximizing value in knapsack

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/knapsack.js
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/knapsack.js
@@ -1,19 +1,22 @@
 // UCSD Algorithms coursera
 // for unsorted data
-let W = 20;
+
+let W = 5; // capacity of knapsack
+
 let n = 3;
-let itemWeights = [[1, 10], [2, 5], [4, 3]];
+// let itemWeights = [[1, 10], [2, 8], [4, 6]];
 let weights = [1, 2, 4];
-let values = [10, 5, 3];
+let values = [10, 8, 6];
 let valuePerWeight = [];
 
-weights.reverse()
-values.reverse()
+weights.reverse();
+values.reverse();
 
 for (let index = 0; index < weights.length; index++) {
    valuePerWeight[index] = values[index] / weights[index];
 }
 console.log(valuePerWeight);
+
 
 // helper to find most valuable item by weight
 function bestItem() {
@@ -22,12 +25,32 @@ function bestItem() {
    for (let index = 0; index < n; index++) {
       if (weights[index] > 0) {
          if (valuePerWeight[index] > maxValueByWeight) {
-            maxValueByWeight = valuePerWeight[index]
-            bestItemIndex = index
+            maxValueByWeight = valuePerWeight[index];
+            bestItemIndex = index;
          }
       }
    }
-   return bestItemIndex
+   return bestItemIndex;
 }
 
-console.log(bestItem());
+
+function knapsack() {
+   amounts = [0, 0, 0];
+   totalValue = 0;
+   for (let index = 0; index < weights.length; index++) {
+      if (W == 0) {
+         return [totalValue, amounts];
+      }
+      i = bestItem();
+      a = Math.min(weights[i], W);
+      totalValue += a * valuePerWeight[i];
+      weights[i] -= a;
+      amounts[i] += a;
+      W = W - a;
+   }
+   return [totalValue, amounts];
+}
+
+
+//console.log(bestItem());
+console.log(knapsack());
