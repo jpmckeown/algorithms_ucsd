1d50c5425cee6b234c5077e52b006524173734c2
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2023-04-29 20:31:48 +0100

    faster maximizing loot with pre-sorted valueByWeight

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/knapsack.js
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/knapsack.js
@@ -9,8 +9,8 @@ let weights = [1, 2, 4];
 let values = [10, 8, 6];
 let valuePerWeight = [];
 
-weights.reverse();
-values.reverse();
+// weights.reverse();
+// values.reverse();
 
 for (let index = 0; index < weights.length; index++) {
    valuePerWeight[index] = values[index] / weights[index];
@@ -52,5 +52,24 @@ function knapsack() {
 }
 
 
+// pre-sorted itesm for decreasing valuePerWeight
+function knapsackFast() {
+   amounts = [0, 0, 0];
+   totalValue = 0;
+   for (let i = 0; i < weights.length; i++) {
+      if (W == 0) {
+         return [totalValue, amounts];
+      }
+      a = Math.min(weights[i], W);
+      totalValue += a * valuePerWeight[i];
+      weights[i] -= a;
+      amounts[i] += a;
+      W = W - a;
+   }
+   return [totalValue, amounts];
+}
+
+
 //console.log(bestItem());
-console.log(knapsack());
+// console.log(knapsack());
+console.log(knapsackFast());
