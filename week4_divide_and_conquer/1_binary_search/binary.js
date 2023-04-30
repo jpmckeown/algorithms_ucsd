8f40cb45e8d678c367d8a4ced07ca123c9201b7c
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2023-04-30 15:41:45 +0100

    Binary search, from week 4

---
+++ week4_divide_and_conquer/1_binary_search/binary.js
@@ -0,0 +1,66 @@
+let arr = [
+   6, 38, 23, 18, 11, 36, 1, 21, 16, 22,
+   7, 3, 33, 32, 30, 28, 34, 4, 9, 2,
+   20, 37, 19, 35, 12, 24, 15, 14, 17, 26,
+   25, 31, 13, 39, 5, 27, 40, 8, 29, 10
+];
+
+// only works if array pre-sorted
+arr.sort(function(a, b){return a - b});
+console.log(arr);
+
+let mid, low, high;
+
+// recursive algorithm 
+function binarySearch(arr, low, high, seek) {
+   if (high < low) return low - 1;
+   let mid = Math.floor(low + (high - low) / 2);
+
+   console.log(mid, low, high);
+
+   if (seek == arr[mid]) {
+      return mid;
+   }
+   else if (seek < arr[mid]) {
+      return binarySearch(arr, low, mid - 1, seek);
+   }
+   else {
+      return binarySearch(arr, mid + 1, high, seek);
+   }
+}
+
+
+// iterative algorithm
+function binarySearchIterate(arr, low, high, seek) {
+   while (low <= high) {
+      mid = Math.floor(low + ((high - low) / 2));
+      console.log(`Mid`, mid, `There`, arr[mid], `Low`, low, `High`, high);
+
+      if (seek === arr[mid]) {
+         return mid;
+      }
+      else if (seek < arr[mid]) {
+         high = mid - 1;
+      }
+      else {
+         low = mid + 1;
+      }
+   }
+   return low - 1;
+}
+
+low = 0;
+high = arr.length - 1;
+let seek = 11;
+
+// let result = binarySearch(arr, 0, arr.length - 1, 9);
+let result = binarySearchIterate(arr, low, high, seek);
+console.log(arr[result], 'Index', result
+);
+
+// let arr = [];
+// while (arr.length < 40) {
+//    let r = Math.floor(Math.random() * 40) + 1;
+//    if (arr.indexOf(r) === -1) arr.push(r);
+// }
+// console.log(arr);
--- week4_divide_and_conquer/1_binary_search/binary_search.js
+++ week4_divide_and_conquer/1_binary_search/binary_search.js
@@ -2,36 +2,37 @@
 
 const readline = require('readline');
 const rl = readline.createInterface({
-    input: process.stdin,
-    terminal: false
+   input: process.stdin,
+   terminal: false
 });
 
 process.stdin.setEncoding('utf8');
 rl.once('line', line => {
-    const arr = line.toString().split(' ').slice(1).map(Number);
+   const arr = line.toString().split(' ').slice(1).map(Number);
 
-    rl.once('line', line => {
-        const keys = line.toString().split(' ').slice(1).map(Number);
-        const result = [];
+   rl.once('line', line => {
+      const keys = line.toString().split(' ').slice(1).map(Number);
+      const result = [];
 
-        for (let key of keys) {
-            result.push(binarySearch(arr, key));
-        }
+      for (let key of keys) {
+         result.push(binarySearch(arr, key));
+      }
 
-        const res = result.join(' ');
-        const maxLength = 50000;
+      const res = result.join(' ');
+      const maxLength = 50000;
 
-        for (let i = 0; i < res.length; i += maxLength) {
-            process.stdout.write(res.slice(i, i + maxLength));
-        }
+      for (let i = 0; i < res.length; i += maxLength) {
+         process.stdout.write(res.slice(i, i + maxLength));
+      }
 
-        process.stdout.write('\n');
-        process.exit();
-    })
+      process.stdout.write('\n');
+      process.exit();
+   });
 });
 
 function binarySearch(arr = [], key) {
-    // write your code here
+   // write your code here
+   console.log(`Binary search`);
 }
 
 module.exports = binarySearch;
